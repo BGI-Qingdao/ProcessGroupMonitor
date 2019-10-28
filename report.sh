70011e3db2ea12d6b448cfa518ee5e2ff7e7ef9d
Lidong <guolidong@genomics.cn>
2019-10-28 18:38:30 +0800

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,9 +3,11 @@
 mem=`tail pglog_* |grep MEM_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
 cpu=`tail pglog_* |grep CPU_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
 t=`tail pglog_* |grep TIME | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
+ct=`tail pglog_* |grep -E "CPU_max|TIME" | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
 
 echo "Total report begin =========="
-echo "MEM_max   $mem    KB."
-echo "CPU_max   $cpu    threads."
-echo "TIME      $t     seconds."
+echo "MEM_max               $mem    KB."
+echo "CPU_max               $cpu    threads."
+echo "TIME                  $t     seconds."
+echo "sum(CPU_max*TIME)     $ct     "
 echo "Total report end =========="
