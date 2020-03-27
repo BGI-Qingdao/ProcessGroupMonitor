04a4c3dd15b06724bd44f8cc56c2ae9834f3417c
Guo Lidong <guolidong@genomics.cn>
2020-03-27 17:13:57 +0800

    bug fixed

--- report.sh
+++ report.sh
@@ -6,8 +6,8 @@ t=`tail pglog_* |grep TIME_total | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'
 ct=`tail pglog_* |grep -E "CPU_max|TIME_total " | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
 
 echo "Total report begin =========="
-echo "MEM_max               $mem    KB."
-echo "CPU_max               $cpu    threads."
+echo "MEM_max               $mem   KB."
+echo "CPU_max               $cpu%  threads."
 echo "TIME                  $t     seconds."
-echo "sum(CPU_max*TIME)     $ct     "
+echo "sum(CPU_max*TIME)*100 $ct    "
 echo "Total report end =========="
