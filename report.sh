94d3f601fae559361cc7a2fe42a49d3b5bcc2860
hanjinglim <76544427+hanjinglim@users.noreply.github.com>
2020-12-25 09:24:44 +0800

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mem=`tail pglog_* |grep MEM_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
-cpu=`tail pglog_* |grep CPU_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
-t=`tail pglog_* |grep TIME_total | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
-ct=`tail pglog_* |grep -E "CPU_max|TIME_total " | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
+mem=`tail -n6 pglog_* |grep MEM_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
+cpu=`tail -n6 pglog_* |grep CPU_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
+t=`tail -n6 pglog_* |grep TIME_total | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
+ct=`tail -n6 pglog_* |grep -E "CPU_max|TIME_total " | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
 
 echo "Total report begin =========="
 echo "MEM_max               $mem   KB."
