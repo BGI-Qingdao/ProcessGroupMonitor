860802c29670a7a2230bafa1dc98b42cde931809
Guo Lidong <guolidong@genomics.cn>
2021-05-11 15:07:13 +0800

    bug

--- report.sh
+++ report.sh
@@ -3,7 +3,7 @@
 mem=`tail -n6 pglog_* |grep MEM_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
 cpu=`tail -n6 pglog_* |grep CPU_max | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
 t=`tail -n6 pglog_* |grep TIME_total | awk 'BEGIN{x=0;}{if($2>x)x=$2;}END{print x;}'`
-ct=`tail -n6 pglog_* |grep -E "CPU_max|TIME_total " | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
+ct=`tail -n6 pglog_* |grep -E "CPU_max|TIME_total" | awk 'BEGIN{CT=0;c=0;}{if($1=="CPU_max"){c=0+$2;}else{t=0+$2; ct=c*t; CT=CT+ct;  c=0; }} END { printf("%d\n", CT ); }'`
 
 echo "Total report begin =========="
 echo "MEM_max               $mem   KB."
